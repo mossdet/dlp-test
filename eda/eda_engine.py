81173f97a6143605ecbb8c9f183e3d897f917f4e
mossdet <mossdet.detector@gmail.com>
2025-04-19 11:11:44 -0600

    add several notebooks to do step wise analysis

--- eda/eda_engine.py
+++ eda/eda_engine.py
@@ -3,34 +3,11 @@ import numpy as np
 from pandas.api.types import is_object_dtype
 import matplotlib.pyplot as plt
 
-# from pandas.api.types import is_numeric_dtype
-# from pandas.api.types import is_object_dtype
-# from pandas.api.types import is_string_dtype
-# from pandas.api.types import is_datetime64_any_dtype as is_datetime
-# from pandas.api.types import is_timedelta64_dtype
-# from pandas.api.types import is_bool_dtype
-# from pandas.api.types import is_categorical_dtype
-# from pandas.api.types import is_integer_dtype
-# from pandas.api.types import is_float_dtype
-# from pandas.api.types import is_unsigned_integer_dtype
-# from pandas.api.types import is_signed_integer_dtype
-# from pandas.api.types import is_floating_dtype
-# from pandas.api.types import is_complex_dtype
-# from pandas.api.types import is_datetime64tz_dtype
-# from pandas.api.types import is_period_dtype
-# from pandas.api.types import is_sparse
-# from pandas.api.types import is_interval_dtype
-# from pandas.api.types import is_object_dtype
-# from pandas.api.types import is_string_dtype
 
-
-# Set the default figure size for matplotlib
-#plt.rcParams['figure.figsize'] = [10, 5]
-
-class EDA_Engine():
+class EDA_Engine:
     """
     A class to perform exploratory data analysis (EDA) on a pandas DataFrame.
-    
+
     Attributes:
         data_df (pd.DataFrame): The DataFrame to analyze.
     """
@@ -52,18 +29,19 @@ class EDA_Engine():
         # Check if the DataFrame has at least one row
         if data_df.shape[0] == 0:
             raise ValueError("DataFrame has no rows")
-        
+
         self.data_df = data_df
 
-        #pd.options.mode.use_inf_as_na = True
+        # pd.options.mode.use_inf_as_na = True
 
-    def overview(self)->None:
+    def overview(self) -> None:
         """
-        Function to provide an overview of the DataFrame.
-        It will print the shape, data types, first few rows, and summary statistics.
+        Function to print an overview of the DataFrame.
+        It will print the shape, data types, first few rows, and other relevant information.
         :return: None
         """
-        
+        print("Overview of the DataFrame:\n")
+
         print(f"Dataset Shape: {self.data_df.shape}")
         print("\n")
 
@@ -89,7 +67,7 @@ class EDA_Engine():
         print("DataFrame index:")
         print(self.data_df.index)
         print("\n")
-       
+
         print("DataFrame nr. unique values:")
         print(self.data_df.nunique())
         print("\n")
@@ -102,7 +80,6 @@ class EDA_Engine():
         print(self.data_df.isna().sum() / self.data_df.shape[0] * 100)
         print("\n")
 
-
     def missing_values(self):
         """
         Function to check missing values in a DataFrame.
@@ -110,12 +87,20 @@ class EDA_Engine():
 
         :return: None
         """
+
+        print("\nMissing values:\n")
+
+        nr_rows_with_nan = self.data_df.isnull().any(axis=1).sum()
+        prctg_rows_with_nan = nr_rows_with_nan/self.data_df.shape[0]*100
+        print(f"Rows with at least one NaN --- Nr. Rows: {nr_rows_with_nan:.2f} --- Percentage: {prctg_rows_with_nan:.2f}%\n")
         
         for i, col_name in enumerate(self.data_df.columns):
             missing_vals = self.data_df[col_name].isna().sum()
             total_vals = self.data_df.shape[0]
             missing_prctg = (missing_vals / total_vals) * 100
-            print(f"{i} Column: {col_name} --- Missing values: {missing_vals} --- Percentage: {missing_prctg:.2f}%")
+            print(
+                f"{i} Column: {col_name} --- Missing values: {missing_vals:.2f} --- Percentage: {missing_prctg:.2f}%"
+            )
 
     def duplicates(self):
         """
@@ -124,37 +109,41 @@ class EDA_Engine():
 
         :return: None
         """
-        
+
+        print("\nDuplicates:")
         duplicates = self.data_df.duplicated().sum()
         print(f"Number of duplicate rows: {duplicates}\n")
-
-
 
     def numerical_values(self):
         """
         Function to check numerical values in a DataFrame.
-        It will print the mean, median, and standard deviation for each numerical column.
+        It will print the measures of statistical dispersion each numerical column.
 
         :return: None
         """
 
-        print("Numerical values (Summarizing Stats %):\n")
+        print("Describe the numerical columns\n")
         for i, col_name in enumerate(self.data_df.columns):
             if not is_object_dtype(self.data_df[col_name]):
-                print(f"{i} Column: {col_name} --- Nr.Uniques: {len(self.data_df[col_name].unique())} --- Min.: {self.data_df[col_name].min():.2f} --- Max.: {self.data_df[col_name].max():.2f} --- Mean: {self.data_df[col_name].mean():.2f} --- Median: {self.data_df[col_name].median():.2f} --- StdDev: {self.data_df[col_name].std():.2f}")
+                print(
+                    f"{i} Column: {col_name} --- Nr.Uniques: {len(self.data_df[col_name].unique())} --- Min.: {self.data_df[col_name].min():.2f} --- Max.: {self.data_df[col_name].max():.2f} --- Mean: {self.data_df[col_name].mean():.2f} --- Median: {self.data_df[col_name].median():.2f} --- StdDev: {self.data_df[col_name].std():.2f}"
+                )
         print("\n")
 
-
-    def categorical_values(self, plot_ok:bool=False)->None:
+    def categorical_values(self, plot_ok: bool = False) -> None:
         """
         Function to check unique categorical values in a DataFrame.
         It will print the unique values and their percentage of occurrence for each categorical column.
         :param plot_ok: Boolean flag to indicate whether to plot the unique values
         :return: None
         """
-        
-        print("Categorical values (Unique entrees %):\n")
-        categrical_cols = self.data_df.select_dtypes(include=['object', 'category']).columns
+
+        print("Describe the categorical columns\n")
+        categrical_cols = self.data_df.select_dtypes(
+            include=["object", "category"]
+        ).columns
+        print(f"Categorical Columns:{categrical_cols}\n")
+
         for col_name in categrical_cols:
             unique_vals_ls = self.data_df[col_name].unique()
             nr_unique_vals = unique_vals_ls.shape[0]
@@ -162,36 +151,43 @@ class EDA_Engine():
             unique_vals_prctg_ls = []
             for unique_val in unique_vals_ls:
                 if pd.isna(unique_val):
-                    prctg_occ = self.data_df[col_name].isna().sum()/self.data_df.shape[0]*100
+                    prctg_occ = (
+                        self.data_df[col_name].isna().sum()
+                        / self.data_df.shape[0]
+                        * 100
+                    )
                 else:
-                    prctg_occ = self.data_df[col_name].str.fullmatch(unique_val, case=True).sum()/self.data_df.shape[0]*100
+                    prctg_occ = (
+                        self.data_df[col_name]
+                        .str.fullmatch(unique_val, case=True)
+                        .sum()
+                        / self.data_df.shape[0]
+                        * 100
+                    )
                 unique_vals_prctg_ls.append(prctg_occ)
                 print(f"{unique_val}: {prctg_occ:.2f}%")
 
             print("\n")
-            print(f"Categorical Columns:{categrical_cols}\n")
 
             if plot_ok:
-                fig = plt.figure(figsize = (5,10))
+                fig = plt.figure(figsize=(5, 10))
                 ax = fig.subplots()
-                patches, texts, pcts = ax.pie(x=unique_vals_prctg_ls, labels=unique_vals_ls, autopct='%.0f%%')
+                patches, texts, pcts = ax.pie(
+                    x=unique_vals_prctg_ls, labels=unique_vals_ls, autopct="%.0f%%"
+                )
                 for i, patch in enumerate(patches):
                     texts[i].set_color(patch.get_facecolor())
-                    ax.set_title(f"Column:{col_name}", fontsize=24, color='black')
-
-
-
-                
+                    ax.set_title(f"Column:{col_name}", fontsize=24, color="black")
 
 
 if __name__ == "__main__":
     # Example usage
     import etl.data_loader as dl
+
     # Load the data using the DataLoader class
-    data_loader = dl.DataLoader('data.csv')
+    data_loader = dl.DataLoader("data.csv")
     data_df = data_loader.load_data()
     # Create an instance of the EDA_Engine class
     eda_engine = EDA_Engine(data_df)
     # Perform EDA
     eda_engine.overview()
-
--- etl/data_loader.py
+++ etl/data_loader.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-class DataLoader():
+
+class DataLoader:
     """
     A utility class for loading and handling data from various file formats.
 
@@ -18,27 +19,30 @@ class DataLoader():
     Methods:
         load_data():
             Loads the data from the specified file using the appropriate Pandas reader function.
-        
+
         get_data_types():
             Returns the data types of the columns in the loaded DataFrame.
 
     Raises:
         ValueError: If the file format is unsupported or if there is an error during data loading.
     """
-    def __init__(self, file_path:str=None):
+
+    def __init__(self, file_path: str = None):
         self.file_path = file_path
         self.data_reader = None
 
-        if file_path.endswith('.csv'):
+        if file_path.endswith(".csv"):
             self.data_reader = pd.read_csv
-        elif file_path.endswith('.parquet'):
+        elif file_path.endswith(".parquet"):
             self.data_reader = pd.read_parquet
-        elif file_path.endswith('.json'):
+        elif file_path.endswith(".json"):
             self.data_reader = pd.read_json
-        elif file_path.endswith('.xlsx'):
+        elif file_path.endswith(".xlsx"):
             self.data_reader = pd.read_excel
         else:
-            raise ValueError("Unsupported file format. Supported formats are: .csv, .json, .xlsx")
+            raise ValueError(
+                "Unsupported file format. Supported formats are: .csv, .json, .xlsx"
+            )
 
         self.data = self.load_data()
 
@@ -50,15 +54,14 @@ class DataLoader():
             return data
         except Exception as e:
             raise ValueError(f"Error loading data: {e}")
-    
+
     def get_data_types(self):
         if self.data is None:
             raise ValueError("Data not loaded. Please load the data first.")
-        
 
         return self.data.dtypes
-        
+
 
 if __name__ == "__main__":
-    data_loader = DataLoader('data.csv')
+    data_loader = DataLoader("data.csv")
     print(data_loader.data.head())
